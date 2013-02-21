b8db804c1269e092c359b13a311ef6261a2e3e33
brandonrodriguez <brodrigu@mines.edu>
2013-02-21 08:13:49 -0700

    added empty java files for junit testing

---
+++ experiment/IntBoardTests.java
@@ -0,0 +1,14 @@
+package experiment;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IntBoardTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
