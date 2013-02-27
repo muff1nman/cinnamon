14f79363c2716736a77cd4aba3d8f4453b20424b
brandonrodriguez <brodrigu@mines.edu>
2013-02-27 16:10:21 -0700

    added .csv file and legend.txt

--- boardTesting/BoardTests.java
+++ boardTesting/BoardTests.java
@@ -3,10 +3,14 @@ package boardTesting;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class BoardTests {
-
+	@BeforeClass
+	public void beforeClass() {
+		// not sure if we need yet, but listed under requirements about chocie between before and beforeclass
+	}
 	@Before
 	public void setUp() throws Exception {
 	}
