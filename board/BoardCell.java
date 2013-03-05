238f1419aa449cd0151e74290ed0a61c4871b0c4
Hunter Lang <hlang@mymail.mines.edu>
2013-03-04 18:44:49 -0700

    boardcell documented

--- board/BoardCell.java
+++ board/BoardCell.java
@@ -1,8 +1,18 @@
+/*
+ * ABSTRACT Class: BoardCell, parent of WalkwayCell and RoomCell
+ * Authors: Brandon Rodriguez, Hunter Lang
+ * This class provides the framework needed for the different types of cells in the game
+ */
 package board;
 
+// BoardCell class body
 public abstract class BoardCell {
+	
+	// Cells require that they have a row and a column
 	private int row;
 	private int column;
+	
+	// "is" functions are set to false then overridden in the appropriate cell to suit that cell's needs
 	public boolean isWalkway() {
 		return false;
 	}
@@ -12,5 +22,7 @@ public abstract class BoardCell {
 	public boolean isDoorway() {
 		return false;
 	}
+	
+	// abstract function draw is used to render the appropriate cell in the gui
 	abstract void draw();
 }
