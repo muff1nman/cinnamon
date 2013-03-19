62813bd234fa04eca067025f6a3d885a80e82c57
dave <davida3141@gmail.com>
2013-03-19 17:40:14 -0600

    added player testing suite

---
+++ playerTests/GameActionTests.java
@@ -0,0 +1,14 @@
+package playerTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameActionTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
