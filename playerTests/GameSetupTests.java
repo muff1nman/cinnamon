f0297780ef806ed2457bdc1bbd196670f3ff17bc
jerphiker <jerphiker@gmail.com>
2013-03-19 18:51:38 -0600

    Added comments for each test that must be added to GameSetupTests

--- playerTests/GameSetupTests.java
+++ playerTests/GameSetupTests.java
@@ -2,28 +2,38 @@ package playerTests;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class GameSetupTests {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@BeforeClass
+	public void setup() {
+		
 	}
 	
+	// Each tests name, color, starting location
 	@Test
 	public void testLoadingPeople() {
-		
+		// Tests person at beginning
+		// Tests arbitrary middle person
+		// Tests person at end
 	}
 	
 	@Test
 	public void testLoadingCards() {
-		
+		// Tests Deck has proper number of cards
+		// Tests deck contains proper number of each type of card
+		// Tests one room
+		// Tests one weapon
+		// Tests one person
 	}
 	
 	@Test
 	public void testDealingCards() {
-		
+		// Tests all cardds are dealt
+		// Tests all players have within 1 card of each other
+		// Tests one card is not given to multiple players
 	}
 
 }
