d4b5eae07974d310099ba947d29ccce24facbba5
dave <davida3141@gmail.com>
2013-03-21 19:52:14 -0600

    target selection tests

--- misc/ComputerPlayer.java
+++ misc/ComputerPlayer.java
@@ -7,6 +7,15 @@ import board.BoardCell;
 
 public class ComputerPlayer extends Player {
 	
+	private char lastRoomVisited;
+	
+	public char getLastRoomVisited() {
+		return lastRoomVisited;
+	}
+
+	public void setLastRoomVisited(char lastRoomVisited) {
+		this.lastRoomVisited = lastRoomVisited;
+	}
 
 	public void pickLocation() {}
 	
--- playerTests/GameActionTests.java
+++ playerTests/GameActionTests.java
@@ -3,6 +3,7 @@ package playerTests;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 import misc.Card;
 import misc.ClueGame;
@@ -19,18 +20,20 @@ import board.BoardCell;
 
 
 public class GameActionTests {
-	private ClueGame game;
-	private Board board;
+	private static ClueGame game;
+	private static Board board;
 	//private Solution solution;
-	private Card mustardCard, whiteCard, knifeCard, pipeCard, kitchenCard, conservatoryCard;
+	private static Card mustardCard, whiteCard, knifeCard, pipeCard, kitchenCard, conservatoryCard;
 	//temp stuff yo. delete this and add legit deal functionality i think.
 	//ououeoeuo
 	//oueiuoeuioeuioeuio
 	
 	@BeforeClass
-	public void setUp() {
+	public static void setUp() {
 		game = new ClueGame();
-		board = new Board();
+		board = new Board("RoomLayout.csv","legend.txt");
+		board.loadConfigFiles();
+		board.calcAdjacencies();
 		mustardCard = new Card("Colonel Mustard", Card.CardType.PERSON);
 		whiteCard = new Card("Mr. White", Card.CardType.PERSON);
 		knifeCard = new Card("knife", Card.CardType.WEAPON);
@@ -72,8 +75,80 @@ public class GameActionTests {
 		//run lots of times to test that rooms are being randomly selected
 		for (int x = 0; x < 100; x++) {
 			BoardCell selection = cpuPlayer.pickLocation(board.getTargets());
-			//if (selected == board.getCellAt(0, 2))
+			if (selection == board.getCellAt(0, 2)) {
+				loc_2++;
+			}
+			else if (selection == board.getCellAt(1, 1)) {
+				loc_27++;
+			}
+			else if (selection == board.getCellAt(2, 0)) {
+				loc_52++;
+			}
+			else {
+				fail("invalid target");
+			}
 		}
+		//check to make sure all location selections were successful
+		assertEquals(100, loc_2 + loc_52 + loc_27);
+		//make sure each target was chosen a reasonable number of times
+		assertTrue(loc_2 > 10);
+		assertTrue(loc_52 > 10);
+		assertTrue(loc_27 > 10);
 	}
-
+	
+	@Test
+	public void testSelectUnvisitedRoom() {
+		ComputerPlayer cpuPlayer = new ComputerPlayer();
+		int loc_14_15 = 0;
+		board.startTargets(board.calcIndex(15, 14), 3);
+		
+		//ensure it picks the correct room every time
+		for(int x = 0; x<100; x++) {
+			cpuPlayer.setLastRoomVisited('L');
+			BoardCell selection = cpuPlayer.pickLocation(board.getTargets());
+			if (selection == board.getCellAt(14, 15)) {
+				loc_14_15++;
+			}
+		}
+		//true if unvisited room was picked every time.
+		assertTrue(loc_14_15 == 100);
+	}
+	
+	@Test
+	public void testSelectRandomIfRoomLastVisited() {
+		ComputerPlayer cpuPlayer = new ComputerPlayer();
+		int loc_19_20 = 0; 
+		int loc_16_21 = 0;
+		int loc_17_20 = 0;
+		int loc_18_19 = 0;
+		//location with only 1 door in range
+		board.startTargets(board.calcIndex(18, 21), 2);
+		cpuPlayer.setLastRoomVisited('O');
+		
+		for( int x = 0; x < 100; x++) {
+			BoardCell selection = cpuPlayer.pickLocation(board.getTargets());
+			if (selection == board.getCellAt(19, 20)) {
+				loc_19_20++;
+			}
+			else if (selection == board.getCellAt(16, 21)) {
+				loc_16_21++;
+			}
+			else if (selection == board.getCellAt(17, 20)) {
+				loc_17_20++;
+			}
+			else if (selection == board.getCellAt(19, 20)) {
+				loc_18_19++;
+			}
+			else fail("invalid targets");
+		}
+		//check to make sure all location selections were successful
+				assertEquals(100, loc_19_20 + loc_16_21 + loc_17_20 + loc_18_19);
+				//make sure each target was chosen a reasonable number of times
+				assertTrue(loc_19_20 > 5);
+				assertTrue(loc_16_21 > 5);
+				assertTrue(loc_17_20 > 5);
+				assertTrue(loc_18_19 > 5);
+	}
 }
+
+
