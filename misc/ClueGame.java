49dd06eeb407923da9c818eeeb0baefc4d1508a0
jerphiker <jerphiker@gmail.com>
2013-03-24 17:41:23 -0600

    Passes loading players test

--- misc/ClueGame.java
+++ misc/ClueGame.java
@@ -1,6 +1,12 @@
 package misc;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import board.BadConfigFormatException;
+import board.Board;
 
 import misc.Card.CardType;
 
@@ -8,17 +14,63 @@ public class ClueGame {
 
 	private Solution solution;
 	
-	private ArrayList<Card> deck = new ArrayList<Card>();
-	private ArrayList<Card> closetCards = new ArrayList<Card>();
-	private ArrayList<ComputerPlayer> cpuPlayers = new ArrayList<ComputerPlayer>();
-	private HumanPlayer Bob = new HumanPlayer();
+	private ArrayList<Card> deck;
+	private ArrayList<Card> closetCards;
+	private ArrayList<ComputerPlayer> cpuPlayers;
+	private HumanPlayer Bob;
 	private Player whosTurn;
+	private Board board;
+	private String legend;
+	private String layout;
+	private String players;
+	private String weapons;
 
-	public ClueGame(String legend, String layout, String players, String weapons) {}
-	public ClueGame() {}
+	public ClueGame(String legend, String layout, String players, String weapons) {
+		this.legend = legend;
+		this.layout = layout;
+		this.players = players;
+		this.weapons = weapons;
+		deck = new ArrayList<Card>();
+		closetCards = new ArrayList<Card>();
+		cpuPlayers = new ArrayList<ComputerPlayer>();
+		Bob = new HumanPlayer();
+		board = new Board(layout, legend);
+	}
+	public ClueGame() {
+		legend = "legend.txt";
+		layout = "RoomLayout.csv";
+		players = "players.txt";
+		weapons = "weapons.txt";
+		deck = new ArrayList<Card>();
+		closetCards = new ArrayList<Card>();
+		cpuPlayers = new ArrayList<ComputerPlayer>();
+		Bob = new HumanPlayer();
+		board = new Board(layout, legend);
+	}
 	
-	public void loadConfigFiles() {}
-	public void loadPeople() {}
+	public void loadConfigFiles() {
+		board.loadConfigFiles();
+		loadPeople();
+		loadDeck();
+	}
+	public void loadPeople() {
+		Scanner peopleFile = null;
+		try {
+			peopleFile = new Scanner(new File(players));
+		} catch (FileNotFoundException e) {
+			System.out.println("Players file not found");
+		}
+		String[] peopleSplit;
+		while(peopleFile.hasNextLine()) {
+			peopleSplit = peopleFile.nextLine().split(",");
+			if(peopleSplit[0].charAt(0) == '+') {
+				Bob = new HumanPlayer(peopleSplit[0].substring(1), peopleSplit[1], Integer.parseInt(peopleSplit[2]), Integer.parseInt(peopleSplit[3]));
+			} else {
+				cpuPlayers.add(new ComputerPlayer(peopleSplit[0], peopleSplit[1], Integer.parseInt(peopleSplit[2]), Integer.parseInt(peopleSplit[3])));
+			}
+		}
+		peopleFile.close();
+	}
 	public void loadDeck() {}
 	
 	public boolean checkAccusation(Solution solution) {
--- misc/Player.java
+++ misc/Player.java
@@ -9,10 +9,7 @@ public class Player {
 	private int column;
 	private int location;	//	?
 	private String name;
-
-	
 	protected ArrayList<Card> knownCards;
-
 	private String color;
 	private ArrayList<Card> cards;
 	
@@ -68,5 +65,44 @@ public class Player {
 	public void giveCard(Card card) {
 		cards.add(card);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result + column;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + row;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Player other = (Player) obj;
+		if (color == null) {
+			if (other.color != null)
+				return false;
+		} else if (!color.equals(other.color))
+			return false;
+		if (column != other.column)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (row != other.row)
+			return false;
+		return true;
+	}
+
 	
+
 }
